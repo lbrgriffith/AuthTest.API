d6b236aa598613459a91a0917de459b775aeae1d
Ricardo Griffith <ricardo.griffith@icloud.com>
2020-10-17 06:09:59 -0300

    [update] Added [Authorize]  attribute and reference to g Microsoft.AspNetCore.Authorization

--- AuthTest.API/Controllers/WeatherForecastController.cs
+++ AuthTest.API/Controllers/WeatherForecastController.cs
@@ -1,13 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 
 namespace AuthTest.API.Controllers
 {
     [ApiController]
+    [Authorize]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
